93924400469eeb8aaead89001b09d1ee5dbadef0
RinaK <g13914kr+git@gm.tsuda.ac.jp>
2015-04-30 12:17:04 +0900

    修正しました！

--- snake-camel/test/snakecamel/SnakeCamelTest.java
+++ snake-camel/test/snakecamel/SnakeCamelTest.java
@@ -1,14 +1,46 @@
 package snakecamel;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
+import snakecamel.SnakeCamelUtil;
 
 import org.junit.Test;
 
 public class SnakeCamelTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void snakeToCamelTest() {
+		SnakeCamelUtil s = new SnakeCamelUtil();
+		String expected = "SoFtWaRe";
+		String actual = s.snakeToCamelcase("so_ft_wa_re");
+		assertThat(actual, is(expected));
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void camelToSnakeTest() {
+		SnakeCamelUtil s = new SnakeCamelUtil();
+		String expected = "so_ft_wa_re";
+		String actual = s.camelToSnakecase("SoFtWaRe");
+		assertThat(actual, is(expected));
+		//fail("Not yet implemented");
 	}
 
+	@Test
+	public void capitalizeTest() {
+		SnakeCamelUtil s = new SnakeCamelUtil();
+		String expected = "Hello";
+		String actual = s.capitalize("hello");
+		assertThat(actual, is(expected));
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void uncapitalizeTest() {
+		SnakeCamelUtil s = new SnakeCamelUtil();
+		String expected = "hello";
+		String actual = s.uncapitalize("Hello");
+		assertThat(actual, is(expected));
+		//fail("Not yet implemented");
+	}
 }
